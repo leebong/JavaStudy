8bf2ed7acd372d8d29b319f9ea3fd362d548c6da
alpo <leesb7755@yahoo.co.kr>
2018-03-29 17:12:51 +0900

    Rect.java Class Modify Complete

--- JavaStudy/src/study/example1/Rect.java
+++ JavaStudy/src/study/example1/Rect.java
@@ -5,11 +5,60 @@ public class Rect {
 	private Point rightDown;
 	private int width;
 	private int height;
+	public Point getLeftUp() {
+		return leftUp;
+	}
+	public void setLeftUp(Point leftUp) {
+		this.leftUp = leftUp;
+	}
+	public Point getRightDown() {
+		return rightDown;
+	}
+	public void setRightDown(Point rightDown) {
+		this.rightDown = rightDown;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
 	
+	public void calWidth() {
+		width = Math.abs(rightDown.getX() - leftUp.getX());		
+	}
 	
+	public void calHeight() {
+		height = Math.abs(rightDown.getY() - leftUp.getY());	
+	}
 	
+	//»ý¼ºÀÚ
+	public Rect() {
+		this.leftUp = new Point(0,10);
+		this.rightDown = new Point(10,0);
+		this.calWidth();
+		this.calHeight();	
+	}
 	
+	public Rect(int left, int up, int right, int down) {
+		this.leftUp = new Point(left,up);
+		this.rightDown = new Point(right,down);
+		this.calWidth();
+		this.calHeight();
+	}
 	
-		
+	public Rect(Point leftUp, Point rightDwon) {
+		this.leftUp = new Point(leftUp);
+		this.rightDown = new Point(rightDown);
+		this.calWidth();
+		this.calHeight();
+	}
+	
 }
 
