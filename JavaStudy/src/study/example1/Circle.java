6aa644a6133da3b3e3430fa5b59031234eef36cc
alpo <leesb7755@yahoo.co.kr>
2018-03-29 15:37:07 +0900

    Java Study.example1 - Circle.java

---
+++ JavaStudy/src/study/example1/Circle.java
@@ -0,0 +1,52 @@
+package study.example1;
+
+public class Circle {
+
+	
+		private int centerX;
+		private int centerY;
+		private double radius;
+		
+			
+		public int getCenterX() {
+			return centerX;
+		}
+		public void setCenterX(int centerX) {
+			this.centerX = centerX;
+		}
+		public int getCenterY() {
+			return centerY;
+		}
+		public void setCenterY(int centerY) {
+			this.centerY = centerY;
+		}
+		public double getRadius() {
+			return radius;
+		}
+		public void setRadius(double radius) {
+			this.radius = radius;
+		}
+
+
+
+		public Circle() {
+			this.setCenter(0,0);
+			this.setRadius(5.0);
+		}
+		public Circle(int x, int y, double r) {
+			this.setCenter(x,y);
+			this.setRadius(10.0);
+		}
+		public Circle(Circle c) {
+			this.setCenter(c.centerX, c.centerY);
+			this.setRadius(c.radius);
+		}
+		public void setCenter(int x, int y) {
+			this.setCenterX(x);
+			this.setCenterY(y);
+		}
+		
+		
+	
+
+}
