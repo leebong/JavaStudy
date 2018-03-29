2d0959c9a86eb35f92e465ef7482c3a8e881e514
leebong <leesb7755@yahoo.co.kr>
2018-03-30 01:12:57 +0900

    자바연습 - MethodReturns(int, double 값을 반환)

---
+++ JavaStudy/ptc/study/practice/MethodReturns.java
@@ -0,0 +1,22 @@
+package study.practice;
+
+public class MethodReturns {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		int result = adder(4,5);
+		System.out.println("4와 5의 합 : " + result);
+		System.out.println("3.5의 제곱 : " + square(3.5));
+				
+	}
+	
+	public static int adder(int num1, int num2) {
+		int addResult = num1 + num2;
+		return addResult;
+		
+	}
+	
+	public static double square(double num) {
+		return num*num;
+	}
+}
