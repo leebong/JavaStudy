839313df7f83bdf8752d53d5ded328cdf02a59ce
alpo <leesb7755@yahoo.co.kr>
2018-03-29 16:45:00 +0900

    Point.java Class Complete

---
+++ JavaStudy/src/study/example1/Point.java
@@ -0,0 +1,28 @@
+package study.example1;
+
+public class Point {
+	private int x;
+	private int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void move(int x, int y) {
+		this.setX(x);
+		this.setY(y);		
+	}
+	
+	public void print() {
+		System.out.println("(" + x + "," + y + ")");
+	}
+
+}
