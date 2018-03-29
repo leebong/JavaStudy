d226cf82ccae60d15e6636ca7c3429bbb999240b
alpo <leesb7755@yahoo.co.kr>
2018-03-29 16:44:12 +0900

    Rect.java Class Modifying

--- JavaStudy/src/study/example1/Rect.java
+++ JavaStudy/src/study/example1/Rect.java
@@ -1,29 +1,14 @@
 package study.example1;
 
 public class Rect {
-	private int x;
-	private int y;
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
+	private Point leftUp;
+	private Point rightDown;
+	private int width;
+	private int height;
 	
-	public void move(int x, int y) {
-		this.setX(x);
-		this.setY(y);		
-	}
 	
-	public void print() {
-		System.out.println("(" + x + "," + y + ")");
-	}
+	
+	
 	
 		
 }
