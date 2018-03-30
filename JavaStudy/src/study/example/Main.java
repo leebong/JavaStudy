75fa8f4e53de21a52be6863451eb0fcde34a92bd
alpo <leesb7755@yahoo.co.kr>
2018-03-30 11:43:18 +0900

    사각형 생성, 크기변경, 이동 메인클래스

--- JavaStudy/src/study/example/Main.java
+++ JavaStudy/src/study/example/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		// 직사각형을 나타내기 위한 기능들을 멤버 메소드로 구현하여
 		// Main 메소드에서 테스트 하세요
 		
-		Circle c1 = new Circle();
+		/*Circle c1 = new Circle();
 		c1.printCircle();
 		c1.moveCenter(10, 10);
 		c1.printCircle();
@@ -23,10 +23,18 @@ public class Main {
 		Rect r = new Rect();
 		r.print();
 		Rect r2 = new Rect(0,0,10,-10);
-		r2.print();
-		Rect r3 = new Rect(new Point(0,0), new Point(10,-10));
+		r2.print(); */
+		
+		Rect r3 = new Rect(new Point(0,0), 
+				new Point(10,-10));
 		r3.print();
-
+		r3.move(3,-3);
+		System.out.println("x방향으로 3만큼, y방향으로" + "-3만큼 이동후");
+		r3.print();
+		r3.resize(5, 5);
+		System.out.println("가로 5, 세로 5로 변경 후");
+		r3.print();
+		
 			
 	}
 	
