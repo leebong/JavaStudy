743941e20138ba411f572be1588fea2588cd7fd1
alpo <leesb7755@yahoo.co.kr>
2018-03-30 16:09:55 +0900

    no message

---
+++ JavaStudy/src/study/overloadTest/Main.java
@@ -0,0 +1,15 @@
+package study.overloadTest;
+
+import study.Number.Number;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		System.out.println(Number.sum(10, 10));
+		System.out.println(Number.sum(10, 10.0));
+		System.out.println(Number.sum(10.0, 10));
+		
+	}
+
+}
